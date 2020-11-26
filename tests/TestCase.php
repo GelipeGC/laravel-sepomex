2752b830a0b70cd3bc01c068485b438ceb37aa73
Felipe Guzman <felipe-guzman.c@hotmail.com>
2020-11-26 05:08:21 +0000

    setup project structure and created firts testunit test

---
+++ tests/TestCase.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests;
+
+use Orchestra\Testbench\Concerns\WithLoadMigrationsFrom;
+
+class TestCase extends \Orchestra\Testbench\TestCase
+{
+/*     use WithLoadMigrationsFrom;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
+    }
+    protected function getPackageProviders($app)
+    {
+        parent::getPackageProviders($app);
+
+        return [
+            'Fguzman\QueryFilterServiceProvider'
+        ];
+    }
+    public function getEnvironmentSetUp($app)
+    {
+        parent::getEnvironmentSetUp($app);
+
+        $app['config']->set([
+            'database.default' => 'querydb',
+        ]);
+
+        $app['config']->set('database.connections.querydb', [
+            'driver' => 'sqlite',
+            'database' => ':memory:'
+        ]);
+    } */
+}
